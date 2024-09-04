ba900bab19a7ac697838cd2c851bae34a184c067
Nitingowda20 <Nitingowda3448@gmail.com>
2024-09-04 14:40:14 +0530

    complete signin PAGE functionality

--- AniLink/ani-conn/src/pages/SignInPage.jsx
+++ AniLink/ani-conn/src/pages/SignInPage.jsx
@@ -1,8 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 export default function SignInPage() {
+  
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    });
+  };  
+  // console.log(formData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      return setErrorMessage("Please fill out all feilds ");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData), // formData is your JSON object
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(error.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-8">
@@ -14,36 +55,63 @@ export default function SignInPage() {
             </span>
             Blog
           </Link>
-          <p className="text-sm mt-5">
+          <p className="text-xl mt-5">
             Join our anime community to stay updated on the latest reviews,
-            discussions, and insights.iSgn in to continue exploring your favorite
-            series and topics.
+            discussions, and insights. Sign in now to start your journey into
+            the world of anime.
           </p>
         </div>
 
         {/* for Right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
             <div>
               <Label value="Your UserName" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="text" placeholder="Password" id="Ppass" />
+              <TextInput
+                type="password"
+                placeholder="***********"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToBlue" type="submit" outline>
-              Sign-In
+            <Button
+              gradientDuoTone="purpleToBlue"
+              type="submit"
+              disabled={loading}
+              outline
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                "Sign in"
+              )}
             </Button>
           </form>
           <div className="">
-            <span>Create new account.!</span>
-            <Link to={"/sign-out"} className="text-blue-500">
-              Sign-up
+            <span>Don't have an account?</span>
+            <Link to={"/sign-in"} className="text-blue-500">
+              Sign up
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
-  );
+  );   
 }
