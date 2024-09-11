9253d2462491293a7b1e1051a43c79e40cdadf47
Nitingowda20 <Nitingowda3448@gmail.com>
2024-09-11 15:15:58 +0530

    made the dashboard private to particular user

--- AniLink/ani-conn/src/App.jsx
+++ AniLink/ani-conn/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Home from './pages/Home'
-// import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import SignInPage from './pages/SignInPage'
 import SignOutPage from './pages/SignOutPage'
 import AboutPage from './pages/AboutPage'
 import HeaderSec from './components/HeaderSec'
 import FooterSec from './components/FooterSec'
+import PrivateRoute from './components/PrivateRoute'
 
 
 export default function App() {
@@ -20,7 +21,9 @@ export default function App() {
         <Route path='/sign-in' element={<SignInPage />}></Route>
         <Route path='/sign-out' element={<SignOutPage />}></Route>
         <Route path='/about' element={<AboutPage />}></Route>
-        {/* <Route path='/dashboard' element={<Dashboard />}></Route> */}
+        <Route element={ <PrivateRoute />} >
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+        </Route>        
         <Route path='/project' element={<Projects />}></Route>
       </Routes>
 
