c5c9195bcbf1f878ffd3d96ba4bb0b9358dc588a
Nitingowda20 <Nitingowda3448@gmail.com>
2024-09-20 16:43:48 +0530

    create a postpage UI

--- AniLink/ani-conn/src/App.jsx
+++ AniLink/ani-conn/src/App.jsx
@@ -9,25 +9,27 @@ import AboutPage from './pages/AboutPage'
 import HeaderSec from './components/HeaderSec'
 import FooterSec from './components/FooterSec'
 import PrivateRoute from './components/PrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <HeaderSec />
-      
+
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/sign-in' element={<SignInPage />}></Route>
-        <Route path='/sign-out' element={<SignOutPage />}></Route>
-        <Route path='/about' element={<AboutPage />}></Route>
-        <Route element={ <PrivateRoute />} >
-          <Route path='/dashboard' element={<Dashboard />}></Route>
-        </Route>        
-        <Route path='/project' element={<Projects />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignInPage />}></Route>
+        <Route path="/sign-out" element={<SignOutPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+        <Route path="/project" element={<Projects />}></Route>
+        <Route path="/create-post" element={<CreatePost />}></Route>
       </Routes>
 
       <FooterSec />
     </BrowserRouter>
-  )
+  );
 }
