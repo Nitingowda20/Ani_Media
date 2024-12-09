c68653b2cfbb163d2738209eebdc4307a918d790
Nitin Gowda <53851231+Nitingowda20@users.noreply.github.com>
2024-12-09 22:50:48 +0530

    Update CreatePost.jsx

--- Tech_Link/tech-conn/src/pages/CreatePost.jsx
+++ Tech_Link/tech-conn/src/pages/CreatePost.jsx
@@ -64,7 +64,7 @@ export default function CreatePost() {
       return;
     }
     try {
-      const res = await fetch("/api/post/create", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/post/create`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
