77d9134c39dc587c91f7e3392d69221c3fbc8380
Nitin Gowda <53851231+Nitingowda20@users.noreply.github.com>
2024-12-09 22:52:49 +0530

    Update Search.jsx

--- Tech_Link/tech-conn/src/pages/Search.jsx
+++ Tech_Link/tech-conn/src/pages/Search.jsx
@@ -37,7 +37,7 @@ export default function Search() {
     const fetchPosts = async () => {
       setLoading(true);
       const searchQuery = urlParams.toString();
-      const res = await fetch(`/api/post/getpost?${searchQuery}`);
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/post/getpost?${searchQuery}`);
       if (!res.ok) {
         setLoading(false);
         return;
@@ -86,7 +86,7 @@ export default function Search() {
     const urlParams = new URLSearchParams(location.search);
     urlParams.set("startIndex", startIndex);
     const searchQuery = urlParams.toString();
-    const res = await fetch(`/api/post/getposts?${searchQuery}`);
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/post/getposts?${searchQuery}`);
     if (!res.ok) {
       return;
     }
