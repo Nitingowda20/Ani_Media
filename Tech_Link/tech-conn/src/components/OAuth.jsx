075017102781f28843e4940e411b9ae1f961b4f4
Nitin Gowda <53851231+Nitingowda20@users.noreply.github.com>
2024-12-09 22:55:49 +0530

    Update OAuth.jsx

--- Tech_Link/tech-conn/src/components/OAuth.jsx
+++ Tech_Link/tech-conn/src/components/OAuth.jsx
@@ -17,7 +17,7 @@ export default function OAuth() {
       const resultFromGoogle = await signInWithPopup(auth, provider);
       //   console.log(resultFromGoogle);
       
-      const res = await fetch("/api/auth/google", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/google`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
