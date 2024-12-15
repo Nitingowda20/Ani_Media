feb4d23d9340fc9f44a875750ec18e897eb3c8ad
Nitingowda20 <Nitingowda3448@gmail.com>
2024-12-15 21:55:48 +0530

    deploy1.0

--- Tech_Link/api/index.js
+++ Tech_Link/api/index.js
@@ -19,12 +19,14 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
-app.use(cors({
-  //replace it with deployed frontend url
-  // origin: "http://localhost:5173",
-  origin :"https://tech-insights-frontend-nit.onrender.com",
-  credentials: true
-}));
+app.use(
+  cors({
+    //replace it with deployed frontend url
+    // origin: "http://localhost:5173",
+    origin: "https://tech-insights-frontend-nit.onrender.com",
+    credentials: true,
+  })
+);
 
 app.get("/health", (req, res) => {
   return res.json("OK");
